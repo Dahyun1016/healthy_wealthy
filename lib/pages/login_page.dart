3127cfb993b7140f82d54790626bed7566e39497
무시무시한다현이 <127650519+Dahyun1016@users.noreply.github.com>
2024-08-26 23:51:50 +0900

    Update login_page.dart

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,58 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
-import 'home_page.dart';
+import 'register_page.dart'; // RegisterPage를 임포트
 
 class LoginPage extends StatelessWidget {
-  final String apiKey; // API 키 추가
+  final String apiKey;
 
   LoginPage({required this.apiKey});
-
-  final TextEditingController _usernameController = TextEditingController();
-  final TextEditingController _passwordController = TextEditingController();
-
-  Future<void> _login(BuildContext context) async {
-    try {
-      final response = await http.post(
-        Uri.parse('http://localhost:3000/login'),
-        headers: {'Content-Type': 'application/json'},
-        body: json.encode({
-          'username': _usernameController.text,
-          'password': _passwordController.text,
-        }),
-      );
-
-      print('로그인 응답 상태 코드: ${response.statusCode}');
-      print('로그인 응답 본문: ${response.body}');
-      print(
-          '로그인 요청 데이터: ${_usernameController.text}, ${_passwordController.text}');
-
-      if (response.statusCode == 200) {
-        final responseBody = json.decode(response.body);
-        String userId = responseBody['userId'];
-        Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(
-              builder: (context) => HomePage(userId: userId, apiKey: apiKey)),
-        );
-      } else {
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text('로그인 실패. 다시 시도해 주세요.')),
-        );
-      }
-    } catch (e) {
-      print('로그인 중 오류 발생: $e');
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('로그인 중 오류가 발생했습니다.')),
-      );
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Login Page'),
+        title: Text('Login'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
@@ -60,18 +18,27 @@ class LoginPage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             TextField(
-              controller: _usernameController,
-              decoration: InputDecoration(labelText: 'Username'),
+              decoration: InputDecoration(labelText: 'ID'),
             ),
             TextField(
-              controller: _passwordController,
               decoration: InputDecoration(labelText: 'Password'),
               obscureText: true,
             ),
             SizedBox(height: 20),
             ElevatedButton(
-              onPressed: () => _login(context),
+              onPressed: () {
+                // 로그인 로직 추가
+              },
               child: Text('Login'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => RegisterPage()),
+                );
+              },
+              child: Text('Sign Up'),
             ),
           ],
         ),
@@ -79,3 +46,5 @@ class LoginPage extends StatelessWidget {
     );
   }
 }
+
+
